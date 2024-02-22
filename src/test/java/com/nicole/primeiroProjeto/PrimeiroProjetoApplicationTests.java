fbdb59eaacbf45201225a37b5825d11a9c397756
Nicole Souza <nicolemadeleines@gmail.com>
2024-02-21 22:13:07 -0300

    primeiro commit

---
+++ src/test/java/com/nicole/primeiroProjeto/PrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicole.primeiroProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
